b6a6217937dbc7a61d85d7e9803bbdd94442414b
GritsaevTim <timgri93@gmail.com>
2023-12-07 19:21:16 +0300

    fix

--- src/datasets/asvspoof2019_dataset.py
+++ src/datasets/asvspoof2019_dataset.py
@@ -16,19 +16,17 @@ class ASVspoof2019Dataset(Dataset):
         suffix = "trn" if part == "train" else "trl"
         with open(f"{dir}/LA/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.{part}.{suffix}.txt", "r") as fin:
             protocols_txt = fin.readlines()
-        self.protocols = dict()
-        for protocol_line in protocols_txt:
+        self.protocols = []
+        for protocol_line in protocols_txt[:limit]:
             protocol_list = protocol_line.split()
-            self.protocols[protocol_list[1]] = 0 if protocol_list[-1] == "spoof" else 1
-
-        self.audio_paths = os.listdir(self.dir_w_audio)[:limit]
+            protocol_list[1]
+            self.protocols.append([protocol_list[1], 0 if protocol_list[-1] == "spoof" else 1])
 
     def __len__(self):
-        return len(self.audio_paths)
+        return len(self.protocols)
 
     def __getitem__(self, idx):
-        audio_name = os.path.basename(self.audio_paths[idx])[:-5]
-        audio, _ = torchaudio.load(os.path.join(self.dir_w_audio, self.audio_paths[idx]))
+        audio, _ = torchaudio.load(os.path.join(self.dir_w_audio, self.protocols[idx][0]))
         if self.max_sec_length is not None:
             audio = audio[:, : self.max_sec_length * DEFAULT_SR]
-        return {"audio": audio, "target": self.protocols[audio_name]}
+        return {"audio": audio, "target": self.protocols[idx][1]}
