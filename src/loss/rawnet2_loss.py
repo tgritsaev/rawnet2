f72ab4aa30a256c0faa38d52fb936c173a642e84
GritsaevTim <timgri93@gmail.com>
2023-12-12 11:35:57 +0300

    fix

--- src/loss/rawnet2_loss.py
+++ src/loss/rawnet2_loss.py
@@ -5,9 +5,8 @@ import torch.nn as nn
 class RawNet2Loss(nn.Module):
     def __init__(self):
         super().__init__()
-        self.weighted_ce_loss = nn.CrossEntropyLoss(weight=torch.Tensor([1, 9]))
+        # self.weighted_ce_loss = nn.CrossEntropyLoss(weight=torch.Tensor([1, 9]))
+        self.weighted_ce_loss = nn.CrossEntropyLoss()
 
     def forward(self, pred, target, **kwargs):
-        # assert pred.shape[0] == target.shape[0], f"{pred.shape=}\t{target.shape}"
-        # assert pred.shape[1] == 2, f"{pred.shape=}"
         return {"loss": self.weighted_ce_loss(pred, target)}
